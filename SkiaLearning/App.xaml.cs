ccca8ff780dfba7b87c89f9b7064d338a4993ae5
abduvokhidabdukhakimov <shrewd2802@gmail.com>
2019-07-31 05:32:06 +0900

    Simple Circle with Tap gesture to toggle fill

--- SkiaLearning/App.xaml.cs
+++ SkiaLearning/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SkiaLearning.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace SkiaLearning
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new TapToggleFillPage();
         }
 
         protected override void OnStart()
---
+++ SkiaLearning/Views/TapToggleFillPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace SkiaLearning.Views
+{
+    public partial class TapToggleFillPage : ContentPage
+    {
+        static bool isFilled = false;
+
+        public TapToggleFillPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            var info = e.Info;
+            var surface = e.Surface;
+            var canvas = surface.Canvas;
+
+            canvas.Clear();
+
+
+            var paint = new SKPaint
+            {
+                Color = Color.Black.ToSKColor(),
+                Style = SKPaintStyle.Stroke,
+                StrokeWidth = 50
+            };
+
+            canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
+
+            if(isFilled)
+            {
+                paint.Style = SKPaintStyle.Fill;
+                paint.Color = Color.HotPink.ToSKColor();
+                canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
+            }
+
+        }
+
+        void OnCanvasViewTapped(object sender, EventArgs args)
+        {
+            isFilled ^= true;
+
+            (sender as SKCanvasView).InvalidateSurface();
+        }
+
+
+    }
+
+}
